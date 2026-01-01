e48e67698305d36380200cfad1bbf169f75a921d
joohwan <piwriw@163.com>
2026-01-01 18:35:12 +0800

    improve: option job doc
    Signed-off-by: joohwan <piwriw@163.com>

--- option.go
+++ option.go
@@ -3,43 +3,77 @@ package chrono
 import "time"
 
 const (
-	AliasOptionName      = "alias"
-	WatchOptionName      = "watch"
+	// AliasOptionName is the name constant for the alias option.
+	// AliasOptionName 是别名选项的名称常量。
+	AliasOptionName = "alias"
+	// WatchOptionName is the name constant for the watch option.
+	// WatchOptionName 是监听选项的名称常量。
+	WatchOptionName = "watch"
+	// WebMonitorOptionName is the name constant for the web monitor option.
+	// WebMonitorOptionName 是 Web 监控选项的名称常量。
 	WebMonitorOptionName = "web_monitor"
-	LimitOptionName      = "limit"
+	// LimitOptionName is the name constant for the limit option.
+	// LimitOptionName 是限制选项的名称常量。
+	LimitOptionName = "limit"
+	// PrometheusOptionName is the name constant for the prometheus option.
+	// PrometheusOptionName 是 Prometheus 选项的名称常量。
 	PrometheusOptionName = "prometheus"
 )
 
 // ScheduleOption is the interface for options in chrono.
 // ScheduleOption 是 chrono 中选项的接口。
 type ScheduleOption interface {
-	// Name Returns the name of the option
-	// 返回选项名称
+	// Name Returns the name of the option.
+	// 返回选项的名称。
+	//
+	// Returns:
+	//	string - The option name / 选项名称
 	Name() string
-	// Enable Returns whether the option is enabled
-	// 返回选项是否启用
+	// Enable Returns whether the option is enabled.
+	// 返回选项是否启用。
+	//
+	// Returns:
+	//	bool - True if enabled, false otherwise / 如果启用返回 true，否则返回 false
 	Enable() bool
 }
 
 // WebMonitorOption represents the web monitor option.
 // WebMonitorOption 表示 Web 监控选项。
 type WebMonitorOption struct {
-	// 是否启用 Web 监控选项
-	// Whether the web monitor option is enabled
+	// enabled indicates whether the web monitor option is enabled.
+	// enabled 表示是否启用 Web 监控选项。
 	enabled bool
-	// Web 监控地址
-	// Web monitor address
+	// address is the web monitor address.
+	// address 是 Web 监控地址。
 	address string
 }
 
+// Name returns the name of the web monitor option.
+// Name 返回 Web 监控选项的名称。
+//
+// Returns:
+//
+//	string - The option name / 选项名称
 func (w *WebMonitorOption) Name() string {
 	return WebMonitorOptionName
 }
 
+// Enable returns whether the web monitor option is enabled.
+// Enable 返回 Web 监控选项是否启用。
+//
+// Returns:
+//
+//	bool - True if enabled, false otherwise / 如果启用返回 true，否则返回 false
 func (w *WebMonitorOption) Enable() bool {
 	return w.enabled
 }
 
+// Address returns the web monitor address.
+// Address 返回 Web 监控地址。
+//
+// Returns:
+//
+//	string - The web monitor address / Web 监控地址
 func (w *WebMonitorOption) Address() string {
 	return w.address
 }
@@ -49,8 +83,8 @@ var _ ScheduleOption = &WebMonitorOption{}
 // AliasOption represents the alias option.
 // AliasOption 表示别名选项。
 type AliasOption struct {
-	// Whether the alias option is enabled
-	// 是否启用别名选项
+	// enabled indicates whether the alias option is enabled.
+	// enabled 表示是否启用别名选项。
 	enabled bool
 }
 
@@ -58,12 +92,20 @@ var _ ScheduleOption = &AliasOption{}
 
 // Name returns the name of the alias option.
 // Name 返回别名选项的名称。
+//
+// Returns:
+//
+//	string - The option name / 选项名称
 func (a *AliasOption) Name() string {
 	return AliasOptionName
 }
 
 // Enable returns whether the alias option is enabled.
 // Enable 返回别名选项是否启用。
+//
+// Returns:
+//
+//	bool - True if enabled, false otherwise / 如果启用返回 true，否则返回 false
 func (a *AliasOption) Enable() bool {
 	return a.enabled
 }
@@ -71,9 +113,11 @@ func (a *AliasOption) Enable() bool {
 // WatchOption represents the watch option.
 // WatchOption 表示监听选项。
 type WatchOption struct {
-	// 是否启用监听选项
-	enabled bool // Whether the watch option is enabled
-	// 监听函数
+	// enabled indicates whether the watch option is enabled.
+	// enabled 表示是否启用监听选项。
+	enabled bool
+	// watchFunc is the watch function for monitoring job events.
+	// watchFunc 是用于监控任务事件的监听函数。
 	watchFunc func(event JobWatchInterface)
 }
 
@@ -81,16 +125,30 @@ var _ ScheduleOption = &WatchOption{}
 
 // Name returns the name of the watch option.
 // Name 返回监听选项的名称。
+//
+// Returns:
+//
+//	string - The option name / 选项名称
 func (w *WatchOption) Name() string {
 	return WatchOptionName
 }
 
 // Enable returns whether the watch option is enabled.
 // Enable 返回监听选项是否启用。
+//
+// Returns:
+//
+//	bool - True if enabled, false otherwise / 如果启用返回 true，否则返回 false
 func (w *WatchOption) Enable() bool {
 	return w.enabled
 }
 
+// WatchFunc returns the watch function.
+// WatchFunc 返回监听函数。
+//
+// Returns:
+//
+//	func(event JobWatchInterface) - The watch function / 监听函数
 func (w *WatchOption) WatchFunc() func(event JobWatchInterface) {
 	return w.watchFunc
 }
@@ -98,46 +156,96 @@ func (w *WatchOption) WatchFunc() func(event JobWatchInterface) {
 // TimeoutOption represents the timeout option.
 // TimeoutOption 表示超时选项。
 type TimeoutOption struct {
-	// 是否启用超时选项
-	enabled bool // Whether the timeout option is enabled
-	// 超时时间
+	// enabled indicates whether the timeout option is enabled.
+	// enabled 表示是否启用超时选项。
+	enabled bool
+	// timeout is the timeout duration.
+	// timeout 是超时时间。
 	timeout time.Duration
 }
 
+// LimitOption represents the limit option.
+// LimitOption 表示限制选项。
 type LimitOption struct {
+	// enabled indicates whether the limit option is enabled.
+	// enabled 表示是否启用限制选项。
 	enabled bool
-	number  int
+	// number is the limit number.
+	// number 是限制数量。
+	number int
 }
 
 var _ ScheduleOption = &LimitOption{}
 
+// Name returns the name of the limit option.
+// Name 返回限制选项的名称。
+//
+// Returns:
+//
+//	string - The option name / 选项名称
 func (l *LimitOption) Name() string {
 	return LimitOptionName
 }
 
+// Enable returns whether the limit option is enabled.
+// Enable 返回限制选项是否启用。
+//
+// Returns:
+//
+//	bool - True if enabled, false otherwise / 如果启用返回 true，否则返回 false
 func (l *LimitOption) Enable() bool {
 	return l.enabled
 }
 
+// Limit returns the limit number.
+// Limit 返回限制数量。
+//
+// Returns:
+//
+//	int - The limit number / 限制数量
 func (l *LimitOption) Limit() int {
 	return l.number
 }
 
+// PrometheusOption represents the prometheus option.
+// PrometheusOption 表示 Prometheus 选项。
 type PrometheusOption struct {
+	// enabled indicates whether the prometheus option is enabled.
+	// enabled 表示是否启用 Prometheus 选项。
 	enabled bool
+	// address is the prometheus address.
+	// address 是 Prometheus 地址。
 	address string
 }
 
 var _ ScheduleOption = &PrometheusOption{}
 
+// Name returns the name of the prometheus option.
+// Name 返回 Prometheus 选项的名称。
+//
+// Returns:
+//
+//	string - The option name / 选项名称
 func (p *PrometheusOption) Name() string {
 	return PrometheusOptionName
 }
 
+// Enable returns whether the prometheus option is enabled.
+// Enable 返回 Prometheus 选项是否启用。
+//
+// Returns:
+//
+//	bool - True if enabled, false otherwise / 如果启用返回 true，否则返回 false
 func (p *PrometheusOption) Enable() bool {
 	return p.enabled
 }
 
+// Address returns the prometheus address.
+// Address 返回 Prometheus 地址。
+//
+// Returns:
+//
+//	string - The prometheus address / Prometheus 地址
 func (p *PrometheusOption) Address() string {
 	return p.address
 }
